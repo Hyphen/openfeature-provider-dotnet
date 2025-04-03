14cef0d327be98e8c214c60da68bd755c8c00a4b
NesCh99 <neschp@gmail.com>
2025-04-03 13:53:37 -0500

    update tests

--- src/Hyphen.OpenFeature.Provider.Tests/HyphenUtilsTests.cs
+++ src/Hyphen.OpenFeature.Provider.Tests/HyphenUtilsTests.cs
@@ -51,7 +51,7 @@ namespace Hyphen.OpenFeature.Provider.Tests
             var result = HyphenUtils.ConvertJsonElementToValue(element);
 
             var list = result.AsList;
-            Assert.Equal("test", list[0].AsString!);
+            Assert.Equal("test", list![0].AsString!);
             Assert.Equal(123, list[1].AsInteger);
             Assert.True(list[2].AsBoolean);
         }
@@ -65,7 +65,7 @@ namespace Hyphen.OpenFeature.Provider.Tests
             var result = HyphenUtils.ConvertJsonElementToValue(element);
 
             var structure = result.AsStructure;
-            Assert.Equal("value1", structure["key1"].AsString!);
+            Assert.Equal("value1", structure!["key1"].AsString!);
             Assert.Equal(123, structure["key2"].AsInteger);
         }
 
@@ -148,5 +148,103 @@ namespace Hyphen.OpenFeature.Provider.Tests
 
             Assert.Null(result);
         }
+
+        [Fact]
+        public void ConvertObjectToValue_WithString_ReturnsValue()
+        {
+            var input = "test string";
+
+            var result = HyphenUtils.ConvertObjectToValue(input);
+
+            Assert.Equal(input, result.AsString);
+        }
+
+        [Fact]
+        public void ConvertObjectToValue_WithNumber_ReturnsValue()
+        {
+            var input = 123;
+
+            var result = HyphenUtils.ConvertObjectToValue(input);
+
+            Assert.Equal(input, result.AsInteger);
+        }
+
+        [Fact]
+        public void ConvertObjectToValue_WithBoolean_ReturnsValue()
+        {
+            var input = true;
+
+            var result = HyphenUtils.ConvertObjectToValue(input);
+
+            Assert.Equal(input, result.AsBoolean);
+        }
+
+        [Fact]
+        public void ConvertObjectToValue_WithList_ReturnsValue()
+        {
+            var input = new List<object> { "string", 123, true };
+
+            var result = HyphenUtils.ConvertObjectToValue(input);
+
+            var list = result.AsList;
+            Assert.Equal("string", list![0].AsString);
+            Assert.Equal(123, list[1].AsInteger);
+            Assert.Equal(true, list[2].AsBoolean);
+        }
+
+        [Fact]
+        public void ConvertObjectToValue_WithDictionary_ReturnsValue()
+        {
+            var input = new Dictionary<string, object>
+            {
+                { "key1", "value1" },
+                { "key2", 123 },
+                { "key3", true }
+            };
+
+            var result = HyphenUtils.ConvertObjectToValue(input);
+
+            var dict = result.AsStructure;
+            Assert.Equal("value1", dict!["key1"].AsString);
+            Assert.Equal(123, dict["key2"].AsInteger);
+            Assert.Equal(true, dict["key3"].AsBoolean);
+        }
+
+        [Fact]
+        public void ConvertObjectToValue_WithNull_ReturnsValue()
+        {
+            object? input = null;
+
+            var result = HyphenUtils.ConvertObjectToValue(input!);
+
+            Assert.True(result.IsNull);
+        }
+
+        [Fact]
+        public void ConvertObjectToValue_WithComplexObject_ReturnsValue()
+        {
+            var input = new
+            {
+                Name = "Test",
+                Age = 30,
+                IsActive = true,
+                Attributes = new Dictionary<string, object>
+                {
+                    { "Height", 180 },
+                    { "Weight", 75 }
+                }
+            };
+
+            var result = HyphenUtils.ConvertObjectToValue(input);
+
+            var structure = result.AsStructure;
+            Assert.Equal("Test", structure!["Name"].AsString);
+            Assert.Equal(30, structure["Age"].AsInteger);
+            Assert.Equal(true, structure["IsActive"].AsBoolean);
+            var attributes = structure["Attributes"].AsStructure;
+            Assert.Equal(180, attributes!["Height"].AsInteger);
+            Assert.Equal(75, attributes["Weight"].AsInteger);
+        }
+
     }
 }
